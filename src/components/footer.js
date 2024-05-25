16f99c4c6e305e0bcb9dd3d8b31acdaa38026933
awatson622 <alexiswatson622@hotmail.com>
2024-05-25 10:32:25 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Follow me on:
+        <a href="https://github.com/yourprofile">GitHub</a>,
+        <a href="https://linkedin.com/in/yourprofile">LinkedIn</a>,
+        <a href="https://stackoverflow.com/users/yourprofile">Stack Overflow</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
 
