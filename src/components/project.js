5427bad4c9acf224f1578a1da2e40d3e8cf5fb70
awatson622 <alexiswatson622@hotmail.com>
2024-05-25 10:32:39 -0700

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const Project = ({ title, image, deployedLink, repoLink }) => {
+  return (
+    <div className="project">
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <a href={deployedLink}>Deployed Application</a>
+      <a href={repoLink}>GitHub Repository</a>
+    </div>
+  );
+};
+
+export default Project;
